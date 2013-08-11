c229ba731d8fd15f6cdce158b4c0bf045fad5409
Bob Blake <robert.j.blake@gmail.com>
2013-08-11 16:06:23 -0400

    Finish half of chapter 10

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,7 +6,8 @@ class UsersController < ApplicationController
 
 	def show
 		@user = User.find(params[:id])
-	end
+	  @microposts = @user.microposts.paginate(page: params[:page])
+  end
 
   def new
   	@user = User.new
@@ -52,13 +53,6 @@ class UsersController < ApplicationController
   end
 
   private
-
-    def signed_in_user
-      unless signed_in?
-        store_location
-        redirect_to signin_url, notice: "Please sign in."
-      end
-    end
 
     def signed_out_user
       if signed_in?
